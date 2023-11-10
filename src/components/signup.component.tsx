f13d751a0ce0cc2e6d7218553b832e326f8273d5
honorati <honorato.bruno@gmail.com>
2023-11-10 14:27:09 -0400

    feat: alert config

--- src/components/signup.component.tsx
+++ src/components/signup.component.tsx
@@ -3,16 +3,16 @@ import { CheckBox } from "./input.checkbox";
 export function SignUp(){
     return (
         <div className="dataCol">            
-            <div className='dataColWhite'>
+            <div className='containerWhite'>
                 <h1>O gerenciador definitivo para sua campanha de RPG!</h1>
-                <p>Tenha acesso a tudo que o Atlas tem à oferecer, basta criar sua conta</p>  
+                <p>Tenha acesso a tudo que o Atlas tem a oferecer, basta criar sua conta</p>  
                 <input type="text" placeholder="Como devemos te chamar?" className="inputText" id="login"/>
                 <input type="text" placeholder="Qual seu e-mail?" className="inputText" id="email"/>
                 <input type="password" placeholder="Escolha uma senha poderosa!" className="inputText" id="password"/>
                 <input type="password" placeholder="Repita sua senha, tem que ser igualzinha!" className="inputText" id="repeatPassword"/>
                 <CheckBox id="mailing" label="Quero receber novidades por email"/>
                 <CheckBox id="notification" label="Quero receber notificações por email"/>
-                <div className="twoColumns">
+                <div className="rowContent">
                     <button className="buttonCancel">Cancelar</button>   
                     <button type="submit">Cadastrar</button>
                 </div>
