75db01120e9a25282407d20beb492f95ef4e81da
honorati <honorato.bruno@gmail.com>
2023-11-13 15:32:41 -0400

    subindo primeira vez

---
+++ src/components/incoming.component.tsx
@@ -0,0 +1,10 @@
+export function Incoming() {
+  return (
+    <div className="containerCentralized">
+      <div className="containerWhite">
+        <h1>Em breve!</h1>
+        <img src="/logo.png" />
+      </div>
+    </div>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './style/App.css'
-import './style/checkbox.css'
-import './style/classes.css'
-import { WYSIWIG } from './components/wysiwig.editor'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./style/App.css";
+import "./style/Checkbox.css";
+import "./style/Classes.css";
+import { Incoming } from "./components/incoming.component";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode> 
-    <WYSIWIG/>
-  </React.StrictMode>, 
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <Incoming />
+  </React.StrictMode>,
+);
