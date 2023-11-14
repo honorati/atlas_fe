2d325d4fd6833c934b7e6dadaceaf018d4480ecb
honorati <honorato.bruno@gmail.com>
2023-11-13 22:19:59 -0400

    deploy test

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Incoming } from "./components/incoming.component";
 import "./style/App.css";
 import "./style/Classes.css";
-import { Incoming } from "./components/incoming.component";
+import "./style/Checkbox.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
