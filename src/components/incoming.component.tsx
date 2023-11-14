b6a8aba2c7694ffd1d73e84528426bfe1b0c6462
honorati <honorato.bruno@gmail.com>
2023-11-13 22:01:12 -0400

    deploy test

--- src/components/incoming.component.tsx
+++ src/components/incoming.component.tsx
@@ -1,10 +1,8 @@
 export function Incoming() {
   return (
     <div className="containerCentralized">
-      <div className="containerWhite">
         <h1>Em breve!</h1>
         <img src="/logo.png" />
-      </div>
     </div>
   );
 }
