b053211ec9b85d021dcedadb883c1b4cdf5e03ae
honorati <honorato.bruno@gmail.com>
2024-05-16 12:15:53 -0400

    data de lançamento

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -12,7 +12,7 @@ const Home: React.FC = () => {
             />
             <hr className="gradient-line" />
             <h2>O gerenciador definitivo para sua campanha de RPG</h2>
-            <p>Disponível em 20/12/2023</p>
+            <p>Disponível em breve</p>
             <hr className="gradient-line" />
             <div className="container">
                <div className="text-left">
@@ -35,43 +35,6 @@ const Home: React.FC = () => {
             <div className="container">
                <div className="frame-left">
                   <img
-                     src="/img2.jpg"
-                     alt="Aventuras"
-                     className="image-left"
-                     draggable="false"
-                  />
-               </div>
-               <div className="text-right">
-                  <h3>Organize suas aventuras</h3>
-                  <p className="text-justify">
-                     Crie e gerencie suas aventuras de RPG de forma rápida e
-                     fácil. Adicione personagens, locais, itens e muito mais!
-                  </p>
-                  <p style={{color: 'red'}}>Em breve!</p>
-               </div>
-            </div>
-            <div className="container">
-               <div className="text-left">
-                  <h3>Crie seus inimigos e oponentes!</h3>
-                  <p className="text-justify">
-                     Inimigos poderosos, hordas gisgantescas, sangue e
-                     destruição para todos os lados! Crie seus monstros e
-                     compartilhe com a comunidade!
-                  </p>
-                  <p style={{color: 'red'}}>Em breve!</p>
-               </div>
-               <div className="frame-right">
-                  <img
-                     src="/img3.jpg"
-                     alt="Bestiario"
-                     className="image-right"
-                     draggable="false"
-                  />
-               </div>
-            </div>
-            <div className="container">
-               <div className="frame-left">
-                  <img
                      src="/img4.jpg"
                      alt="Personagens"
                      className="image-left"
@@ -85,7 +48,44 @@ const Home: React.FC = () => {
                      contrabandista e muito mais! Crie seus personagens mais
                      importantes e impressione seus jogadores!
                   </p>
-                  <p style={{color: 'red'}}>Em breve!</p>
+                  <p style={{ color: "red" }}>Em breve!</p>
+               </div>
+            </div>
+            <div className="container">
+               <div className="text-left">
+                  <h3>Crie seus inimigos e oponentes!</h3>
+                  <p className="text-justify">
+                     Inimigos poderosos, hordas gisgantescas, sangue e
+                     destruição para todos os lados! Crie seus monstros e
+                     compartilhe com a comunidade!
+                  </p>
+                  <p style={{ color: "red" }}>Em breve!</p>
+               </div>
+               <div className="frame-right">
+                  <img
+                     src="/img3.jpg"
+                     alt="Bestiario"
+                     className="image-right"
+                     draggable="false"
+                  />
+               </div>
+            </div>
+            <div className="container">
+               <div className="frame-left">
+                  <img
+                     src="/img2.jpg"
+                     alt="Aventuras"
+                     className="image-left"
+                     draggable="false"
+                  />
+               </div>
+               <div className="text-right">
+                  <h3>Organize suas aventuras</h3>
+                  <p className="text-justify">
+                     Crie e gerencie suas aventuras de RPG de forma rápida e
+                     fácil. Adicione personagens, locais, itens e muito mais!
+                  </p>
+                  <p style={{ color: "red" }}>Em breve!</p>
                </div>
             </div>
          </div>
